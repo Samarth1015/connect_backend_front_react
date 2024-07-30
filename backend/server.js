791f0859562fd96da03d6240ade685a5e553c0ea
samarth <202351125@iiitvadodara.ac.in>
2024-07-30 16:39:45 +0530

    routing

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 
 const port = 8080;
+app.use(cors());
 
+app.get("/api/home", (req, res) => {
+  res.send([{ name: "samarth" }]);
+});
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
 });
